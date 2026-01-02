82c8d5260f602aac86e052756bbb693cb5410329
Omayma-DA <omaymachad4@gmail.com>
2026-01-01 22:40:28 -0500

    Update delivery_analysis.sql
    ADD deliveries table schema

--- delivery_analysis.sql
+++ delivery_analysis.sql
@@ -3,7 +3,20 @@
 -- Dataset: deliveries.csv
 -- Purpose: Analyze late deliveries and performance patterns
 
--- View all records
+-- 1)View all records
 SELECT * FROM deliveries;
 
-
+-- 2)Create table structure
+CREATE TABLE deliveries(
+  order_id INT,
+  customer_id VARCHAR(20),
+  warehouse VARCHAR(50),
+  carrier VARCHAR(50),
+  ship_date DATE,
+  planned_delivery_date DATE,
+  delivery_date DATE,
+  distance_km INT,
+  weight_kg DECIMAL(5,2),
+  shipping_cost DECIMAL(10,2),
+  status VARCHAR(20)
+);
